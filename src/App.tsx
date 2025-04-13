073cf0cf1e9e976315cdbcb0e323d3c1b400a174
Dharun R <dharunmax@gmail.com>
2025-04-13 17:51:46 -0400

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
@@ -7,8 +7,9 @@ import Education from './components/Education';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import { supabase } from "../supabaseClient";
 
-
+const [count, setCount] = useState(null);
 
 function App() {
   useEffect(() => {
@@ -21,6 +22,29 @@ function App() {
     fetch("/.netlify/functions/notify")
       .then((res) => res.json())
       .then((data) => console.log("Email Status:", data.message));
+
+
+    //Count Function
+    const updateVisitorCount = async () => {
+      const { data, error } = await supabase
+        .from("visitor_count")
+        .select("*")
+        .single();
+
+      if (!error && data) {
+        const newCount = data.count + 1;
+
+        await supabase
+          .from("visitor_count")
+          .update({ count: newCount })
+          .eq("id", data.id);
+
+        setCount(newCount);
+      }
+    };
+
+    updateVisitorCount();
+
   }, []);
   
   // useEffect(() => {
